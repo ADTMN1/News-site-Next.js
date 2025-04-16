613943216cea00985730e44e6a8205b42a46d1c9
ADTMN1 <163424703+ADTMN1@users.noreply.github.com>
2025-04-16 04:29:03 -0700

    catch-all fallback route & dealing with multiple segment

--- app/archive/@archive/[[...filter]]/page.js
+++ app/archive/@archive/[[...filter]]/page.js
@@ -1,21 +1,58 @@
-import NewsList from '@/component/news-list';
-import { getAvailableNewsYears, getNewsForYear } from '@/lib/news';
 import Link from 'next/link';
 
-export default async function FilteredNewsPage({ params }) {
-    const filter = params.filter;
-    const links = getAvailableNewsYears();
-    // const news = getNewsForYear(newsYear);
-    console.log(filter)
-    return <header id="archive-header">
-        <nav>
-            <ul>
-                {links.map((link) => (<li key={link}>
-                    <Link href={`/archive/${link}`}>{link}</Link>
-                </li>))}
-            </ul>
-        </nav>
-    </header>
-    //  <NewsList news={news} />
-}
+import NewsList from '@/component/news-list';
+import {
+    getAvailableNewsMonths,
+    getAvailableNewsYears,
+    getNewsForYear,
+    getNewsForYearAndMonth,
+} from '@/lib/news';
 
+export default function FilteredNewsPage({ params }) {
+    const filter = params.filter;
+
+    const selectedYear = filter?.[0];
+    const selectedMonth = filter?.[1];
+
+    let news;
+    let links = getAvailableNewsYears();
+
+    if (selectedYear && !selectedMonth) {
+        news = getNewsForYear(selectedYear);
+        links = getAvailableNewsMonths(selectedYear);
+    }
+
+    if (selectedYear && selectedMonth) {
+        news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+        links = [];
+    }
+
+    let newsContent = <p>No news found for the selected period.</p>;
+
+    if (news && news.length > 0) {
+        newsContent = <NewsList news={news} />;
+    }
+
+    return (
+        <>
+            <header id="archive-header">
+                <nav>
+                    <ul>
+                        {links.map((link) => {
+                            const href = selectedYear
+                                ? `/archive/${selectedYear}/${link}`
+                                : `/archive/${link}`;
+
+                            return (
+                                <li key={link}>
+                                    <Link href={href}>{link}</Link>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </nav>
+            </header>
+            {newsContent}
+        </>
+    );
+}
